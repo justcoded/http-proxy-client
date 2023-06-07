54e51e45d3f99bf46e81fbb884bb49df8ef311cf
Yaroslav <yaroslav.voitenko@justcoded.co>
2023-06-07 13:56:25 +0300

    feat(client-customization): Add the customize settings command

--- src/resources/views/command/customize/dotenv.php
+++ src/resources/views/command/customize/dotenv.php
@@ -1,7 +1,14 @@
-APP_TIMEZONE=<?= config('app.timezone') . PHP_EOL ?>
-WHP_SOCKET_TIMEOUT=<?= config('whp.socket.timeout') . PHP_EOL ?>
-WHP_SOCKET_SELF_SIGNED_SSL=<?= (config('whp.socket.self_signed_ssl') ? 'true' : 'false') . PHP_EOL ?>
-WHP_SOCKET_PROTOCOL_VERSION=<?= config('whp.socket.protocol_version') . PHP_EOL ?>
-WHP_SOCKET_CLIENT_NAME=<?= config('whp.socket.client_name') . PHP_EOL ?>
-WHP_SOCKET_VERSION=<?= config('whp.socket.version') . PHP_EOL ?>
-WHP_SOCKET_FLASH=<?= (config('whp.socket.flash') ? 'true' : 'false') . PHP_EOL ?>
+<?php // !!!NOTICE: the empty lines are MANDATORY ?>
+APP_TIMEZONE=<?= config('app.timezone') ?>
+
+WHP_SOCKET_TIMEOUT=<?= config('whp.socket.timeout') ?>
+
+WHP_SOCKET_SELF_SIGNED_SSL=<?= config('whp.socket.self_signed_ssl') ? 'true' : 'false' ?>
+
+WHP_SOCKET_PROTOCOL_VERSION=<?= config('whp.socket.protocol_version') ?>
+
+WHP_SOCKET_CLIENT_NAME=<?= config('whp.socket.client_name') ?>
+
+WHP_SOCKET_VERSION=<?= config('whp.socket.version') ?>
+
+WHP_SOCKET_FLASH=<?= config('whp.socket.flash') ? 'true' : 'false' ?>
